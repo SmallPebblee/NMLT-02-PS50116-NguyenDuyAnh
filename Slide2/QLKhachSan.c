4bb1ff999b70f6b4913539de455eba677e64b030
NguyenDuyAnh <camqua881@gmail.com>
2026-07-14 12:57:59 +0700

    demo slide2

---
+++ Slide2/QLKhachSan.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int giaNgay = 200
+int giaTuan = 1300
+int soNgay;
+
+int soTuan, soNgayLe;
+printf("Nhap so ngay o khach san: ");
+scanf("%d", &soNgay); //16
+soTuan = soNgay / 7;
+soNgayLe = soNgay % 7;
+
+printf("so ngay o: %d\nSo Tuan: %d\nSo Ngay Le: %d\ntong tien": %d", soNgay, soTuan, soNgayLe)
+    return 0;
+}
